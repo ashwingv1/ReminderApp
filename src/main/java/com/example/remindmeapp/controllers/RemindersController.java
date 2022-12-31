ee98a8407889b5138ae9fe8d4e344c9adfa30309
Ashwin G V <ashwingv1@gmail.com>
2022-12-31 16:27:51 +0530

    Reminder app version 1

---
+++ src/main/java/com/example/remindmeapp/controllers/RemindersController.java
@@ -0,0 +1,45 @@
+package com.example.remindmeapp.controllers;
+
+import com.example.remindmeapp.entity.Reminder;
+import com.example.remindmeapp.services.ReminderDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+public class RemindersController {
+    @Autowired
+    ReminderDaoService reminderDaoService;
+
+    @PostMapping(path = "/reminder")
+    public void createReminder(@RequestBody Reminder reminder){
+        reminderDaoService.createReminder(reminder);
+    }
+
+    @GetMapping(path="/reminder/{reminderId}")
+    public Reminder getReminder(@PathVariable int reminderId){
+        return reminderDaoService.getReminder(reminderId);
+    }
+
+    @GetMapping(path="/reminders/{userId}")
+    public List<Reminder> getAllRemindersForUser(@PathVariable Integer userId){
+        return reminderDaoService.getAllRemindersForUser(userId);
+    }
+
+    @PutMapping(path="/reminder")
+    public void updateReminder(@RequestBody Reminder reminder){
+        reminderDaoService.updateReminder(reminder);
+    }
+
+    @DeleteMapping(path="reminder/{reminderId}")
+    public void deleteReminder(@PathVariable Integer reminderId){
+            reminderDaoService.deleteReminder(reminderId);
+    }
+
+    @DeleteMapping(path="reminders/{userId}")
+    public void deleteReminders(@PathVariable Integer userId){
+        reminderDaoService.deleteReminders(userId);
+    }
+
+    }
